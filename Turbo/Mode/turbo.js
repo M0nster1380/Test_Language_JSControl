7679eadd32ad23d805da228e0be1e8984a56ee5f
M0nster_1380 <56561500+M0nster1380@users.noreply.github.com>
2024-07-11 14:28:42 +0300

    Update turbo.js

--- Turbo/Mode/turbo.js
+++ Turbo/Mode/turbo.js
@@ -24,7 +24,8 @@ CodeMirror.defineMode("turbo", function(config) {
     "float32":true, "float64":true, "int8":true, "int16":true, "int32":true,
     "int64":true, "string":true, "uint8":true, "uint16":true, "uint32":true,
     "uint64":true, "int":true, "uint":true, "uintptr":true, "error": true,
-    "rune":true, "any":true, "comparable":true
+    "rune":true, "any":true, "comparable":true, "class":true, "inherited":true,
+     "import":true, "inclass":true, "public":true
   };
 
   var atoms = {
